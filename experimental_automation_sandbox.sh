a0fe25d56ffa6f04b34193393f2cc5e1c4670ebf
Andrea Cavallini <andrycavallini87@gmail.com>
2025-08-16 07:19:56 +0200

    updated

--- experimental_automation_sandbox.sh
+++ experimental_automation_sandbox.sh
@@ -114,8 +114,7 @@ echo '#!/bin/bash
 chmod 666 /dev/kvm
 /opt/cuckoo3/venv/bin/vmcloak-qemubridge br0 192.168.30.1/24
 chmod u+s /usr/lib/qemu/qemu-bridge-helper
-#su - cuckoo -c "/opt/cuckoo3/venv/bin/cuckoo --debug --cancel-abandoned
-sudo -u cuckoo /opt/cuckoo3/venv/bin/cuckoo --debug --cancel-abandoned"' >> /usr/local/bin/start_cuckoo
+#su - cuckoo -c "/opt/cuckoo3/venv/bin/cuckoo --debug --cancel-abandoned"' >> /usr/local/bin/start_cuckoo
 chmod +x /usr/local/bin/start_cuckoo
 
 # Creating bridge first time
