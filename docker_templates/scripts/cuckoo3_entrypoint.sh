e98c11a2c4c823e7e66be16ba4e1d98a3015b8b9
Andrea Cavallini <andrycavallini87@gmail.com>
2025-11-29 20:50:28 +0100

    first dockerization attempt

---
+++ docker_templates/scripts/cuckoo3_entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Check for first configuration"
+if [ -f /home/cuckoo/.cuckoo/FIRST_DONE ]; then
+  echo "Already done"
+else
+
+fi
+
+chmod 666 /dev/kvm
+vmcloak-qemubridge br0 192.168.30.1/24
+
+# Abilita forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+# Forwarding tra interfacce
+iptables -A FORWARD -i eth0 -o br0 -j ACCEPT
+iptables -A FORWARD -i br0 -o eth0 -j ACCEPT
+
+# NAT per rete interna
+iptables -t nat -A POSTROUTING -s 192.168.30.0/24 -o eth0 -j MASQUERADE
+
+# Avvia il container principale
+exec "$@"
