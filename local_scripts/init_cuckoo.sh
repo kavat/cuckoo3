950b152a0027083708e321122753f04fc4c71dc6
Andrea Cavallini <andrycavallini87@gmail.com>
2025-12-06 19:24:56 +0100

    fixes for dockerization

--- local_scripts/init_cuckoo.sh
+++ local_scripts/init_cuckoo.sh
@@ -3,8 +3,7 @@ touch /home/cuckoo/.cuckoocwd/.cuckoocwd
 /opt/venv/bin/cuckoo createcwd --regen-configs
 /opt/venv/bin/cuckoo getmonitor monitor.zip
 unzip signatures.zip -d ~/.cuckoocwd/signatures/cuckoo/
+/opt/venv/bin/vmcloak-qemubridge br0 192.168.30.1/24
 /opt/venv/bin/cuckoo machine import qemu /home/cuckoo/.vmcloak/vms/qemu
 /opt/venv/bin/cuckoo machine delete qemu example1
 /opt/venv/bin/cuckoomigrate database all
-
-/opt/venv/bin/vmcloak-qemubridge br0 192.168.30.1/24
