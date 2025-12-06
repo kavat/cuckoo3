c906f7f257f025a04e2e8ecb6eb0f722259d13ae
Andrea Cavallini <andrycavallini87@gmail.com>
2025-12-06 17:22:08 +0100

    fixes for dockerization

--- local_scripts/cuckoo3_entrypoint.sh
+++ local_scripts/cuckoo3_entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 chown cuckoo:cuckoo /home/cuckoo -R
+chmod 666 /dev/kvm
+chmod u+s /usr/lib/qemu/qemu-bridge-helper
 
 tail -f /dev/null
--- local_scripts/init_cuckoo.sh
+++ local_scripts/init_cuckoo.sh
@@ -6,3 +6,5 @@ unzip signatures.zip -d ~/.cuckoocwd/signatures/cuckoo/
 /opt/venv/bin/cuckoo machine import qemu /home/cuckoo/.vmcloak/vms/qemu
 /opt/venv/bin/cuckoo machine delete qemu example1
 /opt/venv/bin/cuckoomigrate database all
+
+/opt/venv/bin/vmcloak-qemubridge br0 192.168.30.1/24
