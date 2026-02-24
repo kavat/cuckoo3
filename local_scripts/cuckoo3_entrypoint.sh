ce48ead9b22bcde74b5922584e1800933312d9b7
kavat <andrycavallini87@gmail.com>
2026-02-24 16:58:30 +0100

    fix

--- local_scripts/cuckoo3_entrypoint.sh
+++ local_scripts/cuckoo3_entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+PATH_PYTHON3=$(find / -name "python3" 2>/dev/null | grep venv | xargs dirname)
+
 adduser cuckoo kvm
 chown cuckoo:cuckoo /home/cuckoo -R
 chmod 666 /dev/kvm
 chmod u+s /usr/lib/qemu/qemu-bridge-helper
+$PATH_PYTHON3/vmcloak-qemubridge br0 192.168.30.1/24
 
 tail -f /dev/null
--- local_scripts/init_cuckoo.sh
+++ local_scripts/init_cuckoo.sh
@@ -4,7 +4,6 @@ $PATH_PYTHON3/cuckoo createcwd --update-directories
 $PATH_PYTHON3/cuckoo createcwd --regen-configs
 $PATH_PYTHON3/cuckoo getmonitor monitor.zip
 unzip signatures.zip -d ~/.cuckoocwd/signatures/cuckoo/
-$PATH_PYTHON3/vmcloak-qemubridge br0 192.168.30.1/24
 $PATH_PYTHON3/cuckoo machine import qemu /home/cuckoo/.vmcloak/vms/qemu
 $PATH_PYTHON3/cuckoo machine delete qemu example1
 $PATH_PYTHON3/cuckoomigrate database all
